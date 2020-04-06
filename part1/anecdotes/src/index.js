bf8ba97e6c4a94b54bd35e3a16f7a9f29984a679
yozhikvtumane <toshamatveeev@gmail.com>
2020-04-06 18:37:46 +0200

    Part 1  anecdotes app done, part 1 is finished

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = (props) => {
+	
+	const rand = () => Math.floor(Math.random() * (6 - 0) + 0);
+	const [selected, setSelected] = useState(rand())
+	const [votes, setVote] = useState({0:0, 1:0, 2:0, 3:0, 4:0, 5:0})
+	const [voted, setVoted] = useState(false)
+	
+	const topRated = () => {
+		let maxVal = 0;
+		let maxEl;
+		for (let i in votes) {
+			if (votes[i] > maxVal) {
+				maxVal = votes[i]
+				maxEl = i	
+			}
+		}
+		return maxEl
+	}
+	
+	return (
+		<div>
+			<div className="anecdote">
+				<p>{props.content[selected]}</p>
+				<p>{voted ? votes[selected] + ' votes' : null}</p>
+			</div>
+			<button onClick={() => setSelected(rand())}>Next</button>
+			<button onClick={() => {
+				let newVotes = {...votes}
+				newVotes[selected] += 1
+				setVote({...newVotes})
+				setVoted(true)
+			}}>Vote</button>
+			<h2>Top rated anecdote</h2>
+			<p>
+				{voted ? props.content[topRated()] : null}
+			</p>
+		</div>
+	)
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const anecdotes = [
+	'If it hurts, do it more often',
+	'Adding manpower to a late software project makes it later!',
+	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+	'Premature optimization is the root of all evil.',
+	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render( <App content={anecdotes}/>, document.getElementById('root'));
