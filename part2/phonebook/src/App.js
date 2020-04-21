20af01abc4ba52fe2f03971d10e3481ddd0fcd5a
yozhikvtumane <toshamatveeev@gmail.com>
2020-04-22 00:24:32 +0200

    Part 2 phonebook, exercise 2.11 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
-	const [ persons, setPersons ] = useState([
-		{ name: 'Arto Hellas', number: '040-123456' },
-		{ name: 'Ada Lovelace', number: '39-44-5323523' },
-		{ name: 'Dan Abramov', number: '12-43-234345' },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122' }
-	]) 
-
+	const [ isLoading, setIsloading ] = useState(false)
+	const [ loadingError, setError ] = useState({
+		error: false, errorString: null
+	})
+	const [ persons, setPersons ] = useState([]) 
 	const [ newName, setNewName ] = useState('')
 	const [ newNumber, setNewNumber ] = useState('')
 	const [ filterValue, setFilterValue ] = useState('')
 	const [ filterResult, setFilterResult ] = useState([])
 	const [ filterBool, setFilterBool ] = useState(false)
 	const [ personExists, setPersonExists ] = useState(false)
+	
+	useEffect(()=> {
+		
+		setIsloading(true)
+		
+		fetch('http://localhost:3001/persons')
+			.then( res => res.json())
+			.then( data => {
+				setPersons(data)
+				setIsloading(false)
+			})
+			.catch( err => {
+				setError({
+					error: true, errorString: err.toString()
+				})
+				setIsloading(false)
+			})
+		
+	}, [])
 	
 	const handleNameChange = (e) => {
 		setPersonExists(false)
@@ -56,22 +73,39 @@ const App = () => {
 		setFilterBool(true)
 	}
 	
-	return (
-		<div>
-			<h2>Phonebook</h2>
-			<Filter onChange={handleFilter} filterValue={filterValue} />
-			<h3>Add new number</h3>
-			<PersonForm 
-				onSubmit={submitNewitem}
-				onNameChange={handleNameChange}
-				onNumberChange={handleNumberChange}
-				nameValue={newName}
-				numberValue={newNumber}
-				personExists={personExists}
-			/>
-			<Persons persons={filterBool ? filterResult : persons} />
-		</div>
-	)
+	if (isLoading) {
+		return (
+			<div>
+				<h2>Phonebook</h2>
+				<p>Loading ...</p>
+			</div>
+		)
+	} else if (loadingError.error) {
+		return (
+			<div>
+				<h2>Phonebook</h2>
+				<p>Failed to load phonebook data. {loadingError.errorString}</p>
+			</div>
+		)
+	} else {
+		return (
+			<div>
+				<h2>Phonebook</h2>
+				<Filter onChange={handleFilter} filterValue={filterValue} />
+				<h3>Add new number</h3>
+				<PersonForm 
+					onSubmit={submitNewitem}
+					onNameChange={handleNameChange}
+					onNumberChange={handleNumberChange}
+					nameValue={newName}
+					numberValue={newNumber}
+					personExists={personExists}
+				/>
+				<Persons persons={filterBool ? filterResult : persons} />
+			</div>
+		)
+	}
+
 }
 
 export default App
