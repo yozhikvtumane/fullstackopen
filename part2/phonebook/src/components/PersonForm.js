09e0f66f3338065a544c58bb6a76c003b664833d
yozhikvtumane <toshamatveeev@gmail.com>
2020-06-14 16:40:58 +0200

    Phonebook update

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -13,15 +13,15 @@ const PersonForm = (props) => {
 	
 	return(
 		<form onSubmit={onSubmit}>
-		<div>
-			name: <input required onChange={onNameChange} value={nameValue}/>
-			number: <input required onChange={onNumberChange} value={numberValue}/>
-		</div>
-		<div>
-			<button type="submit">add</button><br />
-			{personExists ? <span style={{color: 'red'}}>{nameValue} already exists in phonebook</span> : null}
-		</div>
-	</form>
+			<div>
+				name: <input required onChange={onNameChange} value={nameValue}/>
+				number: <input required onChange={onNumberChange} value={numberValue}/>
+			</div>
+			<div>
+				<button type="submit">add</button><br />
+				{personExists ? <span style={{color: 'red'}}>{nameValue} already exists in phonebook</span> : null}
+			</div>
+		</form>
 	)
 }
 
